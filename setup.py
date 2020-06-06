3f378136433893baa1699be0d3f877459574fbdb
Ailihamu <elham.abdurusol@gmail.com>
2020-06-06 05:32:28 +0000

    First commit

---
+++ setup.py
@@ -0,0 +1,23 @@
+# TODO: Fill out this file with information about your package
+
+# HINT: Go back to the object-oriented programming lesson "Putting Code on PyPi" and "Exercise: Upload to PyPi"
+
+# HINT: Here is an example of a setup.py file
+# https://packaging.python.org/tutorials/packaging-projects/
+
+
+setup(
+    name="example_", 
+    version="0.0.1",
+    author="Ailihamu Abudouresuli",
+    author_email="elham.abdurusol@gmail.com"
+    description="A small example package",
+    packages=['example_']
+    url="https://github.com/pypa/sampleproject",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.0',
+)
